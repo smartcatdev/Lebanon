87fa32a0980c9f46cda22c412cd81ca0072f1fe0
bilalhassan <info@ottawapc.cagit config --global user.email bilal@smartcat.cagit config --global user.name Bilal>
2017-04-06 13:38:55 -0400

    minor updates to the theme

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -6,28 +6,26 @@
  *
  * @package Lebanon
  */
-?>
-
-<?php if (get_post_thumbnail_id($post->ID)) : ?>
+if (get_post_thumbnail_id($post->ID)) : ?>
     <div id="lebanon-page-jumbotron" class="parallax-window table-display" data-parallax="scroll" style="background-image: url(<?php echo esc_url( wp_get_attachment_url(get_post_thumbnail_id($post->ID)) ); ?>)" >
-        <div class="overlay"></div>
-        
-        <div class="cell-display">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <header class="entry-header">
-                            <?php 
-                            if( get_theme_mod( 'lebanon_post_category', 'on' ) == 'on' ) :
-                                lebanon_entry_footer(); 
-                            endif;
-                            ?>
-                            <?php the_title('<h1 class="text-left entry-title">', '</h1>'); ?>
-                        </header><!-- .entry-header -->                
-                    </div>
+    <div class="overlay"></div>
+
+    <div class="cell-display">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <header class="entry-header">
+                        <?php 
+                        if( get_theme_mod( 'lebanon_post_category', 'on' ) == 'on' ) :
+                            lebanon_entry_footer(); 
+                        endif;
+                        ?>
+                        <?php the_title('<h1 class="text-left entry-title">', '</h1>'); ?>
+                    </header><!-- .entry-header -->                
                 </div>
             </div>
         </div>
+    </div>
 
 
     </div>
@@ -68,7 +66,7 @@
 
                 <div class="entry-content">
                     <?php the_content(); ?>
-                    
+
                     <?php do_action( 'lebanon_after_post_content' ); ?>
 
                     <?php
